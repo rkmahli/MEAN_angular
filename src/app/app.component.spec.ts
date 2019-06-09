5722a541b6c174dbca63d1dac6fcdbc05ffab03f
724447 <724447@cognizant.com>
2019-06-10 02:02:01 +0530

    Toolbar

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-ui'`, () => {
+  it(`should have as title 'mean-ui'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('angular-ui');
+    expect(app.title).toEqual('mean-ui');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular-ui!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to mean-ui!');
   });
 });
